e3de84d4c90d8a0c94ae2160187576e91087c4f3
serghei-pv <pavlov.serghei@web.de>
2021-10-18 14:30:33 +0200

    changed package.json

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    socketHost: "wss://wb-s.herokuapp.com/",
     serverHost: "https://wb-s.herokuapp.com/",
+    socketHost: "wss://wb-s.herokuapp.com/",
     // serverHost: "http://localhost:8100/",
     // socketHost: "http://localhost:8100/",
   },
