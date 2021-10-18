26e007956cc6b43a5056714e56e01b347e938b05
serghei-pv <pavlov.serghei@web.de>
2021-10-18 15:10:40 +0200

    changed package.json

--- server/server.ts
+++ server/server.ts
@@ -93,20 +93,19 @@ nextApp.prepare().then(() => {
     });
   });
 
-  // app.post("/list", (req, res) => {
-  //   res.setHeader("Content-Type", "application/json");
-  //   getQuizAll().then(function (data) {
-  //     if (req.body.id == undefined) {
-  //       res.status(200).send(JSON.stringify(data));
-  //     } else {
-  //       for (let key in data) {
-  //         if (data[key]._id == req.body.id) {
-  //           res.status(200).send(JSON.stringify(data[key]));
-  //         }
-  //       }
-  //     }
-  //   });
-  // });
+  app.post("/list", (req, res) => {
+    getQuizAll().then(function (data) {
+      if (req.body.id == undefined) {
+        res.status(200).send(JSON.stringify(data));
+      } else {
+        for (let key in data) {
+          if (data[key]._id == req.body.id) {
+            res.status(200).send(JSON.stringify(data[key]));
+          }
+        }
+      }
+    });
+  });
 
   const server: Http.Server = Http.createServer(app);
   const io: Server = new Server(server, {
