b0aeb365ca3164a95ce4c8430cb8f69f3f9f829b
serghei-pv <pavlov.serghei@web.de>
2021-10-18 14:49:07 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    // serverHost: "https://wb-s.herokuapp.com/",
-    // socketHost: "wss://wb-s.herokuapp.com/",
-    serverHost: "http://localhost:8100/",
-    socketHost: "http://localhost:8100/",
+    serverHost: "https://wb-s.herokuapp.com/",
+    socketHost: "wss://wb-s.herokuapp.com/",
+    // serverHost: "http://localhost:8100/",
+    // socketHost: "http://localhost:8100/",
   },
 };
