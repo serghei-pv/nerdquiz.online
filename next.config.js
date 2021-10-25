eb24d11628f1dc390fb6b091d574592002d09902
serghei-pv <pavlov.serghei@web.de>
2021-10-25 14:36:17 +0200

    PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
   env: {
     serverHost: "https://wb-s.herokuapp.com/",
     socketHost: "wss://wb-s.herokuapp.com/",
     // serverHost: "http://localhost:8100/",
     // socketHost: "http://localhost:8100/",
   },
-};
+});
